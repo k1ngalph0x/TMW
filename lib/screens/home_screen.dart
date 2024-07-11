14166e1ff041bfd3af00f264bdfdadebc12b261d
k1ng alph0x <shashankintouch@gmail.com>
2024-07-11 22:14:09 +0530

    bottom nav bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  List<BottomNavigationBarItem> _bottomNavbarItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.home), label: 'Home', backgroundColor: Colors.red),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.sports_gymnastics),
+        label: 'BMI',
+        backgroundColor: Colors.red),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.add), label: 'Home', backgroundColor: Colors.red)
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +35,15 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text(
           'T M W',
           style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30.0),
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25.0),
         ),
         elevation: 10.0,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomNavbarItems,
+        currentIndex: _selectedIndex,
+        onTap: _onTap,
+        selectedItemColor: Colors.red[700],
       ),
     );
   }
